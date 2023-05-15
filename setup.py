6ce6fbbfaa224b9964210a69619d5863ec77cf32
Eylon <eylonnaamat@gmail.com>
2023-05-15 14:04:42 +0300

    stam

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='API_Detector_Package',
-    version='0.0.1',
+    version='0.0.2',
     description='A library for API detection layer',
     author='Eylon Naamat & Michael Matveev',
     packages=find_packages(),
